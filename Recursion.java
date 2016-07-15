f7f299b29cf43ed2d4b476e98fd156961588ea49
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-07-15 18:47:47 +0530

    Added Permutation of string example(Recursion)

---
+++ Recursion.java
@@ -0,0 +1,62 @@
+/**
+ *
+ * @author pulkit4tech
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Recursion implements Runnable {
+
+    BufferedReader c;
+    PrintWriter pout;
+    // static long mod = 1000000007;
+
+    public void run() {
+        try {
+            c = new BufferedReader(new InputStreamReader(System.in));
+            pout = new PrintWriter(System.out, true);
+            solve();
+            pout.close();
+        } catch (Exception e) {
+            pout.close();
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Thread(new Recursion()).start();
+    }
+
+    void solve() throws Exception {
+        permutate("bac");
+    }
+
+    void permutate(String input) {
+        int len = input.length();
+        char in[] = input.toCharArray();
+
+        permutate_helper(in, 0, len - 1);
+    }
+
+    void permutate_helper(char in[], int l, int r) {
+        if (l == r) {
+            pout.println(in);
+        } else {
+            for (int i = l; i <= r; i++) {
+                char temp = in[i];
+                in[i] = in[l];
+                in[l] = temp;
+
+                permutate_helper(in, l + 1, r);
+
+                temp = in[i];
+                in[i] = in[l];
+                in[l] = temp;
+            }
+        }
+    }
+
+}
+
