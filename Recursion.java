9b2b2426f22d0cd528862ecdafe66ee50e21b645
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-07-18 17:34:55 +0530

    Added quick sort example (recursion)

--- Recursion.java
+++ Recursion.java
@@ -35,9 +35,8 @@ public class Recursion implements Runnable {
         // combination("abc", 2);
         int no[] = {1, 2, 3, 1, 7, 2};
         Sorting sort = new Sorting(no);
-        no = sort.mergesort();
-        for(int i=0;i<no.length;i++)
-            pout.print(no[i]+" ");
+        sort.quicksort();
+        sort.printArray(no);
     }
 
     class Sorting {
@@ -49,6 +48,53 @@ public class Recursion implements Runnable {
 
             this.data = data;
             len = data.length;
+        }
+        
+        void quicksort(){
+            quicksorthelper(data, 0, len-1);
+        }
+
+        int partition(int arr[], int low, int high) {
+            int pivot = arr[high];
+            int i = (low - 1); // index of smaller element
+            for (int j = low; j <= high - 1; j++) {
+                // If current element is smaller than or
+                // equal to pivot
+                if (arr[j] <= pivot) {
+                    i++;
+
+                    // swap arr[i] and arr[j]
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            // swap arr[i+1] and arr[high] (or pivot)
+            int temp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+
+            return i + 1;
+        }
+
+       
+        void quicksorthelper(int arr[], int low, int high) {
+            if (low < high) {
+                
+                int pi = partition(arr, low, high);
+
+                quicksorthelper(arr, low, pi - 1);
+                quicksorthelper(arr, pi + 1, high);
+            }
+        }
+
+        void printArray(int arr[]) {
+            int n = arr.length;
+            for (int i = 0; i < n; ++i) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
         }
 
         int[] mergesort() {
