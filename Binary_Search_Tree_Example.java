203fc98a74155d0101ccd39efbd2888d94f4ab99
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-08-11 13:09:20 +0530

    Added deleting node in BST example

--- Binary_Search_Tree_Example.java
+++ Binary_Search_Tree_Example.java
@@ -49,7 +49,7 @@ public class Binary_Search_Tree_Example implements Runnable {
 	        tree.insert(70);
 	        tree.insert(60);
 	        tree.insert(80);
-	 
+	        tree.delete(40);
 	        if(tree.search(40)!=null)
 	        	pout.println("Found node 40");
 	        else
@@ -71,6 +71,59 @@ public class Binary_Search_Tree_Example implements Runnable {
 		}
 		
 		Node root;
+		
+		void delete(int key){
+			
+//			1) Node to be deleted is leaf: Simply remove from the tree.
+//        50                            50
+//     /     \         delete(20)      /   \
+//    30      70       --------->    30     70 
+//   /  \    /  \                     \    /  \ 
+// 20   40  60   80                   40  60   80
+// 2) Node to be deleted has only one child: Copy the child to the node and delete the child
+//        50                            50
+//     /     \         delete(30)      /   \
+//    30      70       --------->    40     70 
+//      \    /  \                          /  \ 
+//      40  60   80                       60   80
+// 3) Node to be deleted has two children: Find inorder successor of the node. Copy contents of the inorder successor to the node and delete the inorder successor. Note that inorder predecessor can also be used.
+//        50                            60
+//     /     \         delete(50)      /   \
+//    40      70       --------->    40    70 
+//           /  \                            \ 
+//          60   80                           80
+// The important thing to note is, inorder successor is needed only when right child is not empty. In this particular case, inorder successor can be obtained by finding the minimum value in right child of the node.
+			
+			root = deleteHelper(root,key);
+		}
+		
+		Node deleteHelper(Node node,int key){
+			if(node == null)
+				return root;
+			
+			if(key<node.data)
+				node.left = deleteHelper(node.left, key);
+			else if(key>node.data)
+				node.right = deleteHelper(node.right, key);
+			else{
+				if(node.left == null)
+					return node.right;
+				else if(node.right == null)
+					return node.left;
+				
+				node.data = minVal(node.right);
+				// now delete that successor
+				node.right = deleteHelper(node.right, node.data);
+			}
+			
+			return node;
+		}
+		
+		int minVal(Node root){
+			while(root.left!=null)
+				root = root.left;
+			return root.data;
+		}
 		
 		void insert(int key){
 			root = insertHelper(root,key);
