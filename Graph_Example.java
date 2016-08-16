191d6099a02b44d3dcf0b1553e244915923971fb
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-08-16 07:39:11 +0530

    Added Other Cycle detection algo in graph (Set)

--- Graph_Example.java
+++ Graph_Example.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -43,15 +45,26 @@ public class Graph_Example implements Runnable {
     	Graph g = new Graph(4);
         g.addEdge(0, 1);
         g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
+        g.addEdge(2, 1);
      
         if(g.isCyclic())
-            pout.println("\nGraph contains cycle");
+            pout.println("\nDirected Graph contains cycle");
         else
-            pout.println("\nGraph doesn't contain cycle");
+            pout.println("\nDirected Graph doesn't contain cycle");
+        
+        if(g.isCyclic_SetOperation())
+            pout.println("Undirected Graph contains cycle (using set)");
+        else
+            pout.println("Unidirected Graph doesn't contain cycle (using set)");
+        
+        
+//        The time complexity of the union-find algorithm is O(ELogV). Like directed graphs, we can use DFS to detect cycle in an undirected graph in O(V+E) time. We do a DFS traversal of the given graph. For every visited vertex ‘v’, if there is an adjacent ‘u’ such that u is already visited and u is not parent of v, then there is a cycle in graph. 
+//        If we don’t find such an adjacent for any vertex, we say that there is no cycle. The assumption of this approach is that there are no parallel edges between any two vertices.
+        
+        if(g.isCyclic_Other())
+            pout.println("Graph contains cycle (using other)");
+        else
+            pout.println("Graph doesn't contain cycle (using other)");
         
     }
     
@@ -87,17 +100,105 @@ public class Graph_Example implements Runnable {
     
     class Graph{
     	int V;
+    	ArrayList<Edge> edge;
     	LinkedList<Integer> adj[];
     	
     	Graph(int v){
     		V = v;
+    		edge = new ArrayList<>();
     		adj = new LinkedList[v];
     		for(int i=0;i<v;i++)
     			adj[i] = new LinkedList<>();
     	}
     	
+    	class Edge{
+    		int src,dest;
+    	}
+    	
     	void addEdge(int v,int w){
     		adj[v].add(w);
+    		Edge temp = new Edge();
+    		temp.src = v;
+    		temp.dest = w;
+    		edge.add(temp);
+    	}
+    	
+    	boolean isCyclicUtil_Other(int v, boolean visited[], int parent)
+        {
+            // Mark the current node as visited
+            visited[v] = true;
+            Integer i;
+     
+            // Recur for all the vertices adjacent to this vertex
+            Iterator<Integer> it = adj[v].iterator();
+            while (it.hasNext())
+            {
+                i = it.next();
+     
+                // If an adjacent is not visited, then recur for that
+                // adjacent
+                if (!visited[i])
+                {
+                    if (isCyclicUtil_Other(i, visited, v))
+                        return true;
+                }
+     
+                // If an adjacent is visited and not parent of current
+                // vertex, then there is a cycle.
+                else if (i != parent)
+                    return true;
+            }
+            return false;
+        }
+     
+        // Returns true if the graph contains a cycle, else false.
+        boolean isCyclic_Other()
+        {
+            // Mark all the vertices as not visited and not part of
+            // recursion stack
+            boolean visited[] = new boolean[V];
+     
+            // Call the recursive helper function to detect cycle in
+            // different DFS trees
+            for (int u = 0; u < V; u++)
+                if (!visited[u]) // Don't recur for u if already visited
+                    if (isCyclicUtil_Other(u, visited, -1))
+                        return true;
+     
+            return false;
+        }
+    	
+    	
+    	// valid only for undirected graph
+    	boolean isCyclic_SetOperation(){
+    		int parent[] = new int[V];
+    		Arrays.fill(parent, -1);
+    		
+    		for(int i=0;i<edge.size();i++){
+    			int x = find(parent,edge.get(i).src);
+    			int y = find(parent,edge.get(i).dest);
+    			
+    			if(x==y)
+    				return true;
+    			
+    			union(parent, x, y);
+    		}
+    		
+    		return false;
+    	}
+    	
+    	// Set operation
+    	int find(int parent[],int i){
+    		if(parent[i] == -1)
+    			return i;
+    		
+    		return find(parent,parent[i]);
+    	}
+    	
+    	void union(int parent[],int x,int y){
+    		int xparent = find(parent,x);
+    		int yparent = find(parent,y);
+    		parent[xparent] = yparent;
     	}
     	
     	boolean isCyclic(){
