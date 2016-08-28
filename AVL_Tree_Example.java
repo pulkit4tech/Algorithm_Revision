6b22a94a81ee5d56807d079e83ee1bbfc748ec2e
Pulkit Bhatia <pulkitbhatiais13@gmail.com>
2016-08-28 14:16:29 +0530

    Added deletion in AVL Tree

--- AVL_Tree_Example.java
+++ AVL_Tree_Example.java
@@ -34,23 +34,45 @@ public class AVL_Tree_Example implements Runnable {
     }
     
     private void AVL_example(){
-    	 AVLTree tree = new AVLTree();
-    	 tree.insert(10);
-         tree.insert(20);
-         tree.insert(30);
-         tree.insert(40);
-         tree.insert(50);
-         tree.insert(25);
-  
-         /* The constructed AVL Tree would be
-              30
-             /  \
-           20   40
-          /  \     \
-         10  25    50
+    	AVLTree tree = new AVLTree();
+    	 
+        /* Constructing tree given in the above figure */
+        tree.root = tree.insertHelp(tree.root, 9);
+        tree.root = tree.insertHelp(tree.root, 5);
+        tree.root = tree.insertHelp(tree.root, 10);
+        tree.root = tree.insertHelp(tree.root, 0);
+        tree.root = tree.insertHelp(tree.root, 6);
+        tree.root = tree.insertHelp(tree.root, 11);
+        tree.root = tree.insertHelp(tree.root, -1);
+        tree.root = tree.insertHelp(tree.root, 1);
+        tree.root = tree.insertHelp(tree.root, 2);
+ 
+        /* The constructed AVL Tree would be
+           9
+          /  \
+         1    10
+        /  \    \ 
+       0    5    11 
+      /    /  \
+    -1   2    6
          */
-         pout.println("The preorder traversal of constructed tree is : ");
-         tree.preOrder();
+        pout.println("The preorder traversal of constructed tree is : ");
+        tree.preOrder();
+ 
+        tree.root = tree.deleteNode(tree.root, 10);
+ 
+        /* The AVL Tree after deletion of 10
+           1
+          /  \
+         0    9
+        /     / \  
+       -1    5   11  
+      /  \
+     2    6
+         */
+        pout.println("");
+        pout.println("Preorder traversal after deletion of 10 :");
+        tree.preOrder();
     }
     
     class Node{
@@ -193,6 +215,110 @@ public class AVL_Tree_Example implements Runnable {
             }
     		return node;
     	}
+    	
+    	
+    	Node minValueNode(Node node) {
+            Node current = node;
+     
+            /* loop down to find the leftmost leaf */
+            while (current.left != null) {
+                current = current.left;
+            }
+     
+            return current;
+        }
+     
+        Node deleteNode(Node root, int key) {
+     
+            // STEP 1: PERFORM STANDARD BST DELETE
+            if (root == null) {
+                return root;
+            }
+     
+            // If the key to be deleted is smaller than the root's key,
+            // then it lies in left subtree      
+            if (key < root.data) {
+                root.left = deleteNode(root.left, key);
+            } 
+     
+            // If the key to be deleted is greater than the root's key,
+            // then it lies in right subtree
+            else if (key > root.data) {
+                root.right = deleteNode(root.right, key);
+            } 
+             
+            // if key is same as root's key, then this is the node
+            // to be deleted
+            else {
+     
+                // node with only one child or no child
+                if ((root.left == null) || (root.right == null)) {
+                    Node temp = null;
+                    if (temp == root.left) {
+                        temp = root.right;
+                    } else {
+                        temp = root.left;
+                    }
+     
+                    // No child case
+                    if (temp == null) {
+                        temp = root;
+                        root = null;
+                    } else // One child case
+                    {
+                        root = temp; // Copy the contents of the non-empty child
+                    }
+                } else {
+     
+                    // node with two children: Get the inorder successor (smallest
+                    // in the right subtree)
+                    Node temp = minValueNode(root.right);
+     
+                    // Copy the inorder successor's data to this node
+                    root.data = temp.data;
+     
+                    // Delete the inorder successor
+                    root.right = deleteNode(root.right, temp.data);
+                }
+            }
+     
+            // If the tree had only one node then return
+            if (root == null) {
+                return root;
+            }
+     
+            // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+            root.height = max(height(root.left), height(root.right)) + 1;
+     
+            // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to check whether
+            //  this node became unbalanced)
+            int balance = getBalance(root);
+     
+            // If this node becomes unbalanced, then there are 4 cases
+            // Left Left Case
+            if (balance > 1 && getBalance(root.left) >= 0) {
+                return rightRotate(root);
+            }
+     
+            // Left Right Case
+            if (balance > 1 && getBalance(root.left) < 0) {
+                root.left = leftRotate(root.left);
+                return rightRotate(root);
+            }
+     
+            // Right Right Case
+            if (balance < -1 && getBalance(root.right) <= 0) {
+                return leftRotate(root);
+            }
+     
+            // Right Left Case
+            if (balance < -1 && getBalance(root.right) > 0) {
+                root.right = rightRotate(root.right);
+                return leftRotate(root);
+            }
+     
+            return root;
+        }
     	
     	void preOrder(){
     		preOrderHelp(root);
